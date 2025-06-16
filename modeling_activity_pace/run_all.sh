602f77bf0613666be253e04f2d67ce9cca4b9be9
jpasdeloup <jpasdeloup@deezer.com>
2025-06-16 10:27:34 +0200

    Reactive all steps

--- modeling_activity_pace/run_all.sh
+++ modeling_activity_pace/run_all.sh
@@ -8,28 +8,28 @@ poetry run python modeling_activity_pace/compute_time_series.py
 echo "****************************************"
 echo "2. Dictionary Learning algorithm is run "
 echo "****************************************"
-#poetry run python modeling_activity_pace/compute_dictionary.py
+poetry run python modeling_activity_pace/compute_dictionary.py
 
 echo "**********************************************************"
 echo "3. Selection of the best iteration in dictionary learning "
 echo "**********************************************************"
-#poetry run python modeling_activity_pace/choose_dictionary.py
+poetry run python modeling_activity_pace/choose_dictionary.py
 
 echo ""
 echo "***********************************************************"
 echo "4. Computes baselines scores and scores of PACE embeddings "
 echo "***********************************************************"
-#poetry run python modeling_activity_pace/compute_baselines.py
+poetry run python modeling_activity_pace/compute_baselines.py
 
 echo ""
 echo "******************************************"
 echo "5. Plots logistic regression coefficients "
 echo "   and related statistical reports        "
 echo "******************************************"
-#poetry run python modeling_activity_pace/analyse_models.py
+poetry run python modeling_activity_pace/analyse_models.py
 
 echo -e ""
 echo "******************************"
 echo "6. Saves the plot of Figure 1 "
 echo "******************************"
-#poetry run python modeling_activity_pace/make_fig1.py
+poetry run python modeling_activity_pace/make_fig1.py
